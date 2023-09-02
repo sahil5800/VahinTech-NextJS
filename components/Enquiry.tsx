eeff19fc9f6e9252138d37c668ab798b4a746203
Sahil Raj <69668540+sahil5800@users.noreply.github.com>
2023-09-02 15:25:10 +0530

    Update Enquiry.tsx

--- components/Enquiry.tsx
+++ components/Enquiry.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { TbArrowDownRight } from "react-icons/tb";
+import { MdConnectWithoutContact } from "react-icons/md";
 import Link from "next/link";
 const Enquiry = () => {
     return(
@@ -26,8 +26,8 @@ const Enquiry = () => {
         <button className="w-52 h-12 font-medium text-xl uppercase bg-transparent flex items-center justify-center gap-1 hover:bg-yellow-600 hover:text-black mt-6 border rounded-full group">
           Contact
           <div className="w-5 h-5 relative overflow-hidden">
-            <TbArrowDownRight className="text-2xl -mt-[1px] absolute top-0 -translate-x-5 -translate-y-5 text-black group-hover:translate-x-0 group-hover:translate-y-0 duration-300" />
-            <TbArrowDownRight className="text-2xl -mt-[1px] absolute top-0 translate-x-0 translate-y-0 text-white group-hover:translate-x-5 group-hover:translate-y-5 duration-300" />
+            <MdConnectWithoutContact className="text-2xl -mt-[1px] absolute top-0 -translate-x-5 -translate-y-5 text-black group-hover:translate-x-0 group-hover:translate-y-0 duration-300" />
+            <MdConnectWithoutContact className="text-2xl -mt-[1px] absolute top-0 translate-x-0 translate-y-0 text-yellow-100 group-hover:translate-x-5 group-hover:translate-y-5 duration-300" />
           </div>
         </button>
         </Link>
